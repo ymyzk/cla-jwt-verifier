92342a2e739550c965d8512f347339629bdfdf99
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2020-05-09 14:59:22 +0900

    Add tests to models

--- src/models.rs
+++ src/models.rs
@@ -52,8 +52,59 @@ pub(crate) struct JWKSet {
 }
 
 impl JWKSet {
+    /// Returns a JWK with the given kid if it exists.
+    /// Otherwise, returns None.
+    /// If there multiple keys with the same kid,
+    /// there's no guarantee on which one is returned.
     pub(crate) fn find(&self, kid: &str) -> Option<&JWK> {
         self.keys.iter().find(|jwk| jwk.kid == Some(kid.into()))
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::{JWK, JWKSet, KeyAlgorithm, KeyType};
+
+    #[tokio::test]
+    async fn test_find_jwk_finds_key() {
+        let jwk1 = JWK {
+            kty: KeyType::RSA,
+            use_: Some("sig".to_string()),
+            alg: Some(KeyAlgorithm::RS256),
+            kid: Some("key1".to_string()),
+            n: "x66ZeMvBm8o0qAiKjFsMCVcc34nd".to_string(),
+            e: "AQAB".to_string(),
+        };
+        let jwk2 = JWK {
+            kty: KeyType::RSA,
+            use_: Some("sig".to_string()),
+            alg: Some(KeyAlgorithm::RS256),
+            kid: Some("key2".to_string()),
+            n: "Yadafddsa8o0qAidfafad1321r8s".to_string(),
+            e: "AQAB".to_string(),
+        };
+        let jwks = JWKSet { keys: vec![jwk1.to_owned(), jwk2.to_owned()] };
+        assert_eq!(jwks.find("key1"), Some(&jwk1));
+        assert_eq!(jwks.find("key2"), Some(&jwk2));
+    }
+
+    #[tokio::test]
+    async fn test_find_jwk_not_found() {
+        let jwk = JWK {
+            kty: KeyType::RSA,
+            use_: Some("sig".to_string()),
+            alg: Some(KeyAlgorithm::RS256),
+            kid: Some("key1".to_string()),
+            n: "x66ZeMvBm8o0qAiKjFsMCVcc34nd".to_string(),
+            e: "AQAB".to_string(),
+        };
+        let jwks = JWKSet { keys: vec![jwk] };
+        assert!(jwks.find("key2").is_none());
+    }
+
+    #[tokio::test]
+    async fn test_find_jwk_when_empty() {
+        let jwks = JWKSet { keys: vec![] };
+        assert!(jwks.find("kid").is_none());
+    }
+}
